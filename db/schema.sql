e264e7b9d762b0073603efa544e0b52de400683f
Lendo <wesreedbusiness@gmail.com>
2023-06-07 14:25:38 -0500

    inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
+use employee_db;
+
 CREATE TABLE departments (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(30) NOT NULL,
+    `name` VARCHAR(30) UNIQUE NOT NULL
 );
--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const {
     createConnection
 } = require('./config/dbConfig.js');
 
+const inquirer = require('inquirer');
+
+
 async function main() {
     const connection = await(createConnection(buildConnectionOptions()));
     
+    const { createPromptModule } = inquirer;
+    const prompt = createPromptModule()
+
+    const selectedOption = await prompt([
+        {
+        type: "list",
+        name: "menuOption",
+        choices: ['Get All Departments', 'Create a Department'],
+        },
+]);
+
+    console.log(selectedOption)
+
     const [departments] = await connection.execute(`SELECT * FROM departments;`,[])
     console.table(departments)
 }
